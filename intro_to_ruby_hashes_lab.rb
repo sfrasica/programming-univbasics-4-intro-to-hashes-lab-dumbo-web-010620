3be7de7cf0183316433841d581f96608c321f445
Steven Frasica <sfrasica1@gmail.com>
2019-12-28 08:44:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,9 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  number_hash = {
+    :id => 11
+  }# return a hash with a key :id assigned to positive integer
 end
 
 def my_hash_creator(key, value)
