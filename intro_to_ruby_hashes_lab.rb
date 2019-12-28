05b0f40d67211af68f9176b400bdb44827c4aaab
Steven Frasica <sfrasica1@gmail.com>
2019-12-28 08:55:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ end
 
 def my_hash_creator(key, value)
   create_hash = {
-    key: value
+    key: "value"
   }# return a hash that includes the key and value parameters passed into this method
   create_hash(pokemon, "pikachu")
 end
